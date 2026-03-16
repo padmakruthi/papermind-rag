44646b29530f9f75905b16ef0de7b60505e26608
B P S Kruthi <aes16kruthi16srinivas@gmail.com>
2026-03-16 20:34:56 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -4,7 +4,7 @@ Local embeddings + FAISS + HuggingFace LLM
 """
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
